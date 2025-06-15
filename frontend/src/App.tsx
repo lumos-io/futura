55fd35a4591bd50f6b16d921182d16ca89b27f1c
Davide Berdin <davideberdin@gmail.com>
2025-06-15 11:57:12 +0200

    frontend and makefile setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+// import MainDashboard from "./app/dashboard/dashboard";
+import LoginPage from "./app/login/login";
+
+function App() {
+  return (
+    <>
+      <LoginPage></LoginPage>
+      {/* <MainDashboard></MainDashboard> */}
+    </>
+  );
+}
+
+export default App;
